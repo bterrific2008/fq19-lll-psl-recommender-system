8790e137c10765eacba930774128731c256ff4d2
Bryan Tor <bterrific2008@gmail.com>
2019-11-04 11:24:50 -0800

    Made more data engineering changes, working off a tutorial

--- iid.py
+++ iid.py
@@ -4,17 +4,131 @@ import math
 import os
 
 import pandas
+import numpy as np
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 
 def getData():
+    """
+    https://github.com/csaluja/JupyterNotebooks-Medium/blob/master/Book%20Recommendation%20System.ipynb?source=post_page-----5ec959c41847----------------------
+
+    :return:
+    """
+
     bookData = pandas.read_csv('BX-Books.csv', error_bad_lines=False, sep=';', encoding='utf-8')
-    bookData = bookData.drop(columns=["Image-URL-S", "Image-URL-M", "Image-URL-L"])
+    # remove the useless book cover columns
+    bookData.drop(columns=["Image-URL-S", "Image-URL-M", "Image-URL-L"], inplace=True)
+    bookData.columns = ["ISBN", "bookTitle", "bookAuthor", "yearOfPublication", "publisher"]
+
     userData = pandas.read_csv('BX-Users.csv', error_bad_lines=False, encoding="utf-8", sep=";")
+    userData.columns = ['userId', 'userLocation', 'userAge']
+
     bookRatingData = pandas.read_csv('BX-Book-Ratings.csv', error_bad_lines=False, encoding="utf-8",
                                      sep=";")
+    bookRatingData.columns = ['userId', 'ISBN', 'bookRating']
+
+
+    def bookDataYear(bookData):
+        """
+
+        :param bookData:
+        :return:
+        """
+
+        # ISBN 0789466953 is improperly formatted. Fix it
+        bookData.loc[bookData.ISBN == "0789466953", 'yearOfPublication'] = 2000
+        bookData.loc[
+            bookData.ISBN == "0789466953", 'bookTitle'] = "DK Readers: Creating the X-Men, How Comic Books Come to Life (Level 4: Proficient Readers)"
+        bookData.loc[bookData.ISBN == "0789466953", 'bookAuthor'] = "James Buckley"
+        bookData.loc[bookData.ISBN == "0789466953", 'publisher'] = "DK Publishing Inc"
+
+        # ISBN 078946697X is improperly formatted. Fix it
+        bookData.loc[bookData.ISBN == "078946697X", 'yearOfPublication'] = 2000
+        bookData.loc[
+            bookData.ISBN == "078946697X", 'bookTitle'] = "DK Readers: Creating the X-Men, How It All Began (Level 4: Proficient Readers)"
+        bookData.loc[bookData.ISBN == "078946697X", 'bookAuthor'] = "Michael Teitelbaum"
+        bookData.loc[bookData.ISBN == "078946697X", 'publisher'] = "DK Publishing Inc"
+
+        # ISBN 2070426769 is improperly formatted. Fix it
+        bookData.loc[
+            bookData.ISBN == '2070426769', "bookTitle"] = "Peuple de ciel, suivi de 'Les Bergers"
+        bookData.loc[
+            bookData.ISBN == '2070426769', "bookAuthor"] = "Jean-Marie Gustave Le ClÃ?Â©zio"
+        bookData.loc[bookData.ISBN == '2070426769', "publisher"] = "Gallimard"
+        bookData.loc[bookData.ISBN == '2070426769', "yearOfPublication"] = "2003"
+
+        # Format yearOfPublications to be numeric data
+        bookData.yearOfPublication = pandas.to_numeric(bookData.yearOfPublication, errors='coerce')
+
+        # Our dataset is from 2004, so its unlikely that we'll have data from earlier than 2005
+        #   Set all publication years that are 0 or later than 2005 as the average year
+        # TODO: is setting this stuff as average a good idea?
+        bookData.loc[(bookData.yearOfPublication > 2005) | (
+                bookData.yearOfPublication == 0), 'yearOfPublication'] = np.NAN
+        bookData.yearOfPublication.fillna(round(bookData.yearOfPublication.mean()), inplace=True)
+
+        # set the dtype as int32
+        bookData.yearOfPublication = bookData.yearOfPublication.astype(np.int32)
+
+        return bookData
+
+    bookData = bookDataYear(bookData)
+
+    def publisher(bookData):
+        """
+
+        :param bookData:
+        :return:
+        """
+
+        # if there are books with no listed publisher, then set them to be "other"
+        bookData.publisher.fillna('other', inplace=True)
+        return bookData
+
+    bookData = publisher(bookData)
+
+    def age(userData):
+        """
+
+        :param userData:
+        :return:
+        """
+
+        # its unlikely that people under the age of 12 and people over the age of 90 are leaving
+        #   meaningful reviews. We should filter these out
+        userData.loc[(userData.userAge > 90) | (userData.userAge < 12), 'age'] = np.nan
+
+        # TODO: should we just remove all reviews from users under the age of 12 and over the age
+        #  of 90, or should we replace them with the average age?
+        userData.userAge.dropna(inplace=True)
+        # userData.userAge.fillna(userData.userAge.mean(), inplace=True)
+
+        # set data type as int
+        userData.userAge = userData.userAge.astype(np.int32)
+
+        return userData
+
+    # remove users too young and too old
+    userData = age(userData)
+
+    def dataExists(bookRatingData, bookData, userData):
+        """
+
+        :param bookRatingData:
+        :return:
+        """
+
+        # only keep ratings that we have books for
+        bookRatingData = bookRatingData[bookRatingData.ISBN.isin(bookData.ISBN)]
+
+        # only keep ratings that we have users for
+        bookRatingData = bookRatingData[bookRatingData.userId.isin(userData.userId)]
+
+        return bookRatingData
+
+    bookRatingData = dataExists(bookRatingData, bookData, userData)
 
     masterData = pandas.merge(bookData, bookRatingData, on="ISBN", how='outer')
     masterData = pandas.merge(masterData, userData, on="User-ID", how='outer')
@@ -31,7 +145,7 @@ def getData():
 
     values = masterData[['ISBN', 'Book-Rating']]
     features = masterData[
-        ['ISBN', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher', 'User-ID',
+        ['ISBN', 'bookTitle', 'bookAuthor', 'yearOfPublication', 'publisher', 'User-ID',
          'Location', 'Age']]
 
     # Clean up the data a bit.
